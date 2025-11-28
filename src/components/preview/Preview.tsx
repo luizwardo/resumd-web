f42f2f9e94808b1d7350d93d68e37607e3179acb
André Rocco <99764757+andrerocco@users.noreply.github.com>
2025-11-28 15:40:16 -0300

    style: add light and dark mode support (preferred scheme based)

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -1,9 +1,9 @@
 import { createSignal, type Accessor } from "solid-js";
 import clsx from "clsx";
 import { exportAsPdf } from "./exportPdf";
+// Components
 import ZoomControl from "./ZoomControl";
 import PreviewPages from "./PreviewPages";
-import { IoDownloadOutline } from "solid-icons/io";
 
 export default function Preview(props: { class: string; html: Accessor<string>; css: Accessor<string> }) {
     const [zoom, setZoom] = createSignal(100);
@@ -13,24 +13,28 @@ export default function Preview(props: { class: string; html: Accessor<string>; 
     }
 
     return (
-        <div class={clsx(props.class, "bg-system-primary relative flex flex-col")}>
+        <div class={clsx(props.class, "relative flex flex-col")}>
             <div class="flex-1 overflow-auto">
                 <div style={{ zoom: `${zoom()}%`, height: "100%" }}>
                     <PreviewPages html={props.html()} css={props.css()} />
                 </div>
             </div>
 
-            <div class="absolute top-3 right-0 left-0 flex justify-between px-3.5">
-                <ZoomControl zoom={zoom} setZoom={setZoom} />
+            <div class="absolute top-3 right-0 left-0 flex items-center justify-between gap-3 px-3.5">
+                <div>
+                    <ZoomControl zoom={zoom} setZoom={setZoom} />
+                </div>
 
-                <button
-                    class="bg-blue outline-blue focus-active:outline-2 flex h-9 cursor-pointer items-center rounded-full pl-3.5 font-medium tracking-tight text-white outline-offset-2 backdrop-blur-md active:outline-2"
-                    onClick={handleExport}
-                    title="Export PDF"
-                >
-                    <p>Export as PDF</p>
-                    <IoDownloadOutline class="mr-3 ml-2 size-5" />
-                </button>
+                <div class="flex flex-1 justify-end gap-3">
+                    <button
+                        class="bg-blue outline-blue focus-active:outline-2 flex h-9 cursor-pointer items-center rounded-full px-3.5 font-medium tracking-tight text-white outline-offset-2 backdrop-blur-md active:outline-2"
+                        onClick={handleExport}
+                        title="Export PDF"
+                    >
+                        <p>Export as PDF</p>
+                        {/* <IoDownloadOutline class="mr-3 ml-2 size-5" /> */}
+                    </button>
+                </div>
             </div>
         </div>
     );
